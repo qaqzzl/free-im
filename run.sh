b4afb424a598c235e1f9d6dabfb8e0289371e1e1
zerozz <erppcc@163.com>
2021-03-26 00:10:46 +0800

    😴run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 function check(){
-    if test $( pgrep -f $1 | wc -l ) -neq 0
+    if test $( pgrep -f $1 | wc -l ) -ne 0
     then
-        nohup ./$1 -c ~/free-im/free.yaml > http_app.out 2>&1 &
+        echo "结束进程: $1"
+        killall $1
     fi
+    nohup ./$1 -c ~/free-im/free.yaml > http_app.out 2>&1 &
 }
 
 go build ./cmd/http_app
 go build ./cmd/logic
 go build ./cmd/tcp_conn
 
+check http_app
+check logic
+check tcp_conn
