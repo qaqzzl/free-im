8d4cd963667713f79ebc384da80d38f8a0c2440f
zerozz <erppcc@163.com>
2021-05-06 23:51:44 +0800

    dao

--- internal/http_app/dao/dynamic.go
+++ internal/http_app/dao/dynamic.go
@@ -1,6 +1,15 @@
 package dao
 
+import "free-im/internal/http_app/model"
+
 type dynamic struct {
 }
 
 var Dynamic = new(dynamic)
+
+func (d *dynamic) Create(m model.Dynamic) (dynamic_id int64, err error) {
+	result := Dao.DB().Create(&m)
+	err = result.Error
+	dynamic_id = m.DynamicId
+	return
+}
--- internal/http_app/service/dynamic.go
+++ internal/http_app/service/dynamic.go
@@ -1,12 +1,20 @@
 package service
 
-import "free-im/internal/http_app/model"
+import (
+	"free-im/internal/http_app/dao"
+	"free-im/internal/http_app/model"
+)
 
 type DynamicService struct {
 }
 
 // 创建动态
-func (s DynamicService) Create(m model.Dynamic) (dynamic_id int64, err error) {
-
+func (s *DynamicService) Create(m model.Dynamic) (dynamic_id int64, err error) {
+	dynamic_id, err = dao.Dynamic.Create(m)
 	return
+}
+
+// 动态列表
+func (s *DynamicService) DynamicList() {
+
 }
