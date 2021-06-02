77d52731cb24cf604ac7c205142de93d8375d413
zerozz <erppcc@163.com>
2021-06-02 23:59:37 +0800

    http app api

--- internal/http_app/api/app.go
+++ internal/http_app/api/app.go
@@ -23,6 +23,13 @@ func AppNewVersionGet(c *gin.Context) {
 		info["version_download"] = ""
 		info["version_download_page"] = "https://www.pgyer.com/freeim"
 		info["version_description"] = ""
+	default:
+		info["version_code"] = 3
+		info["is_must"] = 1 // 是否必须更新
+		info["version_name"] = "2.0.0"
+		info["version_download"] = ""
+		info["version_download_page"] = "https://www.pgyer.com/freeim"
+		info["version_description"] = "全新版本\n新增群聊功能\n修复已知BUG"
 	}
 	http.RespOk(c, info, "ok")
 }
