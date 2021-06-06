d3d60f1013db83b7fee9277c4a670cebce6e881f
zerozz <erppcc@163.com>
2021-06-06 23:40:56 +0800

    http 状态吗

--- pkg/http/http_code.go
+++ pkg/http/http_code.go
@@ -1,6 +1,8 @@
 package http
 
 const (
+	HTTP_CODE_OK                  = 0   // 成功
+	HTTP_CODE_ERROR               = 500 // 服务器错误
 	HTTP_CODE_ACCOUNT_TOKEN_ERROR = 401 // 重新登陆
 	HTTP_CODE_SMS_ERROR           = 405 // 短信验证码错误
 )
