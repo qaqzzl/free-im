5e12dc92352acaa85f6f74bd6676cc2b90242887
qaqzz <erppcc@163.com>
2022-05-14 13:38:43 +0800

    http Cors

--- api/http_app/app.go
+++ api/http_app/app.go
@@ -4,6 +4,7 @@ import (
 	"free-im/config"
 	app_http "free-im/internal/http_app/api"
 	http2 "free-im/pkg/http"
+	"free-im/pkg/logger"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
@@ -20,6 +21,9 @@ func StartHttpServer() {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(Cors())
+
 	r.Any("", func(c *gin.Context) {
 		c.String(http.StatusOK, "free-im v1.0")
 	})
@@ -94,5 +98,39 @@ func authorizedMiddleware() gin.HandlerFunc {
 
 		c.Next()
 
+	}
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin") //请求头部
+		if origin != "" {
+			//接收客户端发送的origin （重要！）
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			//服务器支持的所有跨域请求的方法
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+			//允许跨域设置可以返回其他子段，可以自定义字段
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+			// 允许浏览器（客户端）可以解析的头部 （重要）
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+			//设置缓存时间
+			c.Header("Access-Control-Max-Age", "172800")
+			//允许客户端传递校验信息比如 cookie (重要)
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		//允许类型校验
+		if method == "OPTIONS" {
+			c.JSON(http.StatusOK, "ok!")
+		}
+
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Sugar.Error("Panic info is: %v", err)
+			}
+		}()
+
+		c.Next()
 	}
 }
